aa3a95fbfb3a300cc62a54f09df00aa4dbc89388
sanfrancis <46870034+sanfrancis1@users.noreply.github.com>
2022-07-07 17:06:12 +0530

    Delete cvms directory

--- cvms/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="row">
-                            <div class="col-md-12">
-                                <div class="copyright">
-                                    <p>CVMS 2018. All rights reserved.</p>
-                                </div>
-                            </div>
-                        </div>
